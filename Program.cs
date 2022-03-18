8b6a333200e5eba7edfdd5c767f89685f985cd55
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-03-18 08:11:44 -0700

    work

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@ namespace QuestionGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Austin is dumb");
         }
+        
+         public static int AskQuestion(Question question);
+         {
+            if (question.answers == null) throw new ArgumentNullException("List of answers may not be null");
+            if (question.answers.Count == 0) throw new ArgumentException("There should be at least 1 answer in the list");
+            int index = 1;
+            Console.WriteLine($"{question.question}");
+            foreach (string answers in question.answers)
+            {
+                Console.WriteLine($"{index}. {answers}");
+                index = index + 1;
+
+            }
+         }
         
     }
 

797cac6a6897b161bd655753cf38a0ce6ddb109b
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-03-23 13:01:06 -0700

    work

--- Program.cs
+++ Program.cs
@@ -1,28 +1,58 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 namespace QuestionGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Austin is dumb");
+            Console.WriteLine("Do you think Austin Ransford is cool");
+            MakeUserSmart("Enter yes or no");
         }
-        
-         public static int AskQuestion(Question question);
-         {
-            if (question.answers == null) throw new ArgumentNullException("List of answers may not be null");
-            if (question.answers.Count == 0) throw new ArgumentException("There should be at least 1 answer in the list");
-            int index = 1;
-            Console.WriteLine($"{question.question}");
-            foreach (string answers in question.answers)
-            {
-                Console.WriteLine($"{index}. {answers}");
-                index = index + 1;
 
+
+
+        //Display the question to the user
+        //Loop through each answer and display it
+        //Using the GetValidAnswer method get and return the users response
+        /// <summary>
+        /// This displays the question and all of the answers and prompts the user to choose one the answers and returns the answer.
+        /// </summary>
+        /// <param name="question">The question that is being asked</param>
+        /// <returns>It returns the answer to the player</returns>
+        public static string MakeUserSmart(string prompt)
+        {
+
+            List<string> YESRESPONSES = new List<string>();
+            YESRESPONSES = File.ReadAllLines("YES-Responses.txt").ToList();
+
+            List<string> NORESPONSES = new List<string>();
+            NORESPONSES = File.ReadAllLines("NO-Responses.txt").ToList();
+
+            Console.WriteLine($"{prompt}");
+            string input = Console.ReadLine().ToLower();
+            if (input != "no")
+            {
+                Console.Error.WriteLine($"{NORESPONSES}");
             }
-         }
+            else if (input == "yes")
+            {
+                Console.WriteLine($"{YESRESPONSES}");
+            }
+
+            while (input != "no" || input != "yes");
+            do
+            {
+                Console.WriteLine("INVALID ANSWER");
+                Console.WriteLine("Ill ask again. Do you think Austin Ransford is cool. ANSWER YES OR NO");
+            }
         
+            return input;
+
+        }
+
     }
 
 }
